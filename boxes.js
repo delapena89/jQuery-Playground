75333379493ab5cde2591c5ae014b0fc4e8ae11e
Luis <delapenl@gmail.com>
2015-07-28 19:51:14 -0600

    finished the 2nd exercise

--- boxes.js
+++ boxes.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+$('document').ready(function() {
+	console.log("ready");
+	$("#secretBox").css("background-color", "white");
+	$("#secretBox").html("<h1>secret box!</h1>");
+	$("#secretBox").css("color", "black");
+	$("#row1").children().attr("class", "box");
+	$("#row1").children().addClass("boxType3");
+	$(".boxType1").css("background-color","white");
+	$("#row2>:lt(2)").css("visibility","hidden");
+	$(".box").css("width", "2px"); {
+	$("#secretBox").css("width","154px");	
+	}
+});	
+
+
+
+
+
